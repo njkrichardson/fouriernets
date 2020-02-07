3335ae8ef44afa5d2afe13630a4691241177cdc0
Nick Richardson <nrichardson@unify.id>
2020-02-06 23:57:19 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from distutils.core import setup
+import setuptools
 
-setup(name='fouriernets',
+setuptools.setup(name='fouriernets',
       version='0.1dev',
       description='Neural networks for density estimation on data with circular symmetries.',
       author='Nick Richardson',
       author_email='nrichardson@hmc.edu',
       url='https://github.com/njkrichardson/fouriernets',
-      packages=['distutils', 'distutils.command'],
-      long_description=open('README.txt').read(), 
+      long_description=open('README.md').read(), 
       packages=setuptools.find_packages(),
       classifiers=[
         "Programming Language :: Python :: 3",
